0054a52efdc957cdbbe71b3df84d7375cdc2fbb4
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-19 10:23:41 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/src/components/ui/button"
+
+export default function page() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+    
+    
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+    
+    
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <h1 className="text-red-900 p-2">Page ddf</h1>
+            <Button className="cursor-pointer">Mon bouton</Button>
+        </>
+    )
+}
