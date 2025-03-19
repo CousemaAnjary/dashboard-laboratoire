bba6ce7b0a1fa4efd8fae3f5ffb71f3f2688bd76
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-19 22:13:54 +0300

    ajout du schéma de connexion avec validation des champs et intégration de nouveaux composants d'interface utilisateur

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,19 +1,155 @@
+"use client"
+import Link from "next/link"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+import { useState, useTransition } from "react"
+import { LoginSchema } from "@/src/lib/schemas/auth"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { AtSign, Eye, EyeOff, Loader, LockKeyhole } from "lucide-react"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+
+
 export default function LoginForm() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+    const router = useRouter()
+    const [loading, setLoading] = useState(false)
+    const [isPending, startTransition] = useTransition()
+    const [showPassword, setShowPassword] = useState(false)
+
+    const form = useForm({
+        resolver: zodResolver(LoginSchema),
+    })
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+    const handleLogin = async () => {
+        console.log("Login")
+    }
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-        </>
+        <div className="relative w-full max-w-md p-8">
+            <h1 className="mb-2 font-spaceGrotesk text-2xl font-medium ">Se connecter</h1>
+            <p className="mb-4 ">
+                Connectez-vous à votre compte pour accéder à votre espace personnel
+            </p>
+            <p className="mb-4 font-spaceGrotesk  text-sm text-muted-foreground">
+                Vous n&apos;avez pas de compte ? Inscrivez-vous en cliquant <Link href="/register" className="text-cyan-700 underline">ici</Link>
+            </p>
+
+            <Form {...form} >
+                <form onSubmit={form.handleSubmit(handleLogin)}>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="font-inter text-sm">Adresse email </FormLabel>
+                                        <FormControl>
+                                            {/* Conteneur pour l'input et l'icône */}
+                                            <div className="relative ">
+                                                <Input type="email" {...field} placeholder="exemple@gmail.com" className="bg-white ps-10 font-inter shadow-sm dark:bg-zinc-950" />
+                                                {/* Icône */}
+                                                <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/95 peer-disabled:opacity-50">
+                                                    <AtSign size={16} strokeWidth={2} aria-hidden="true" />
+                                                </div>
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage className="font-inter" />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-9 gap-2">
+                            <div className="col-span-8 grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem >
+                                            <div className="flex flex-row items-center justify-between space-y-0">
+                                                <FormLabel className="font-inter text-sm">Mot de passe</FormLabel>
+                                                <Link href="/forgot-password" className="font-spaceGrotesk text-sm text-muted-foreground hover:underline">
+                                                    Mot de passe oublié ?
+                                                </Link>
+                                            </div>
+                                            <FormControl>
+                                                {/* Conteneur pour l'input et l'icône */}
+                                                <div className="relative ">
+                                                    <Input {...field} type={showPassword ? "text" : "password"} placeholder="Entrez votre mot de passe" className="bg-white ps-10 font-inter shadow-sm dark:bg-zinc-950" />
+                                                    {/* Icône */}
+                                                    <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 text-muted-foreground/95 peer-disabled:opacity-50">
+                                                        <LockKeyhole size={16} strokeWidth={2} aria-hidden="true" />
+                                                    </div>
+                                                </div>
+                                            </FormControl>
+                                            <FormMessage className="font-inter" />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="mb-1 grid">
+                                <Button type="button" variant="outline" size={"icon"} className="mt-8" onClick={() => setShowPassword(!showPassword)}>
+                                    {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+                                </Button>
+                            </div>
+                        </div>
+                        {/* <div className="grid gap-2">
+                            <FormField
+                                control={form.control}
+                                name="rememberMe"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+                                        <FormControl>
+                                            <Checkbox
+                                                className="data-[state=checked]:bg-blue-900 dark:data-[state=checked]:bg-white"
+                                                checked={field.value}
+                                                onCheckedChange={field.onChange}
+                                            />
+                                        </FormControl>
+                                        <div className="space-y-1 leading-none">
+                                            <FormLabel className="font-inter">
+                                                Se souvenir de moi
+                                            </FormLabel>
+                                        </div>
+                                    </FormItem>
+                                )}
+                            />
+                        </div> */}
+
+                        <div className="grid">
+                            <Button type="submit" className="w-full bg-blue-900 font-inter hover:bg-blue-950 dark:bg-white" disabled={loading}>
+                                {loading || isPending ? (
+                                    <>
+                                        <Loader className="mr-2 size-4 animate-spin" />
+                                        Veuillez patienter
+                                    </>
+                                ) : (
+                                    "Connexion"
+                                )}
+
+                            </Button>
+                        </div>
+                        <div className="relative">
+                            <div className="relative flex justify-center text-xs uppercase">
+                                <span className="bg-background px-2 font-inter text-muted-foreground">Ou continuer avec</span>
+                            </div>
+                        </div>
+
+                        {/* <Social /> */}
+                    </div>
+                </form>
+            </Form>
+        </div>
     )
 }
