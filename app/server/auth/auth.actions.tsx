51af38ee74679886ea80f33ebeb40d920aeb1a84
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-20 01:08:52 +0300

    ajout des gestionnaires d'authentification pour l'enregistrement, la connexion et la vérification par email

---
+++ app/server/auth/auth.actions.tsx
@@ -0,0 +1,10 @@
+"use server"
+
+// Enregistre un utilisateur
+export async function register() { }
+
+// Connecte un utilisateur
+export async function login() { }
+
+// Vérifie un utilisateur par email avec OTP
+export async function verifyEmail() { }
