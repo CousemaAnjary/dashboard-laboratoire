2b5d11429fba6914e0761ca42037c1bb12c25184
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-19 20:15:09 +0300

    ajout de la structure de la page avec Navbar et sections principales

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,37 @@
-import { Button } from "@/src/components/ui/button"
+"use client"
+
+import Navbar from "@/src/components/navbar"
+
 
 export default function page() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-            <h1 className="text-red-900 p-2">Page ddf</h1>
-            <Button className="cursor-pointer">Mon bouton</Button>
-        </>
+        <div className="flex min-h-screen flex-col">
+            {/* <GridPattern width={50} height={50} x={-1} y={-1} strokeDasharray={"4 2"} className={cn("absolute inset-0 [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]")} /> */}
+            {/* En-tête */}
+            <header>
+                <Navbar />
+            </header>
+
+            {/* Contenu principal */}
+            <main className="grow">
+
+            </main>
+
+            {/* Pied de page */}
+            <footer></footer>
+        </div >
     )
 }
