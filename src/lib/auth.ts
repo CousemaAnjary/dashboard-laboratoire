457586bb7aec82a953090a1fcae26f815941d413
CousemaAnjary <anjaryabdillah@gmail.com>
2025-03-21 03:25:29 +0300

    ajout de la prise en charge de l'envoi d'e-mails de vérification OTP avec le plugin emailOTP dans la configuration d'authentification

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import db from "@/db/drizzle"
 import * as schema from "@/db/schema"
 import { betterAuth } from "better-auth"
 import { nextCookies } from "better-auth/next-js"
+import { emailOTP, openAPI } from "better-auth/plugins"
+import { sendEmail } from "@/app/server/auth/email.actions"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 
 
@@ -25,5 +27,24 @@ export const auth = betterAuth({
         }
     },
 
-    plugins: [nextCookies()]
+    plugins: [
+        openAPI(), // Expose an OpenAPI schema at /api/auth/reference
+        emailOTP({
+            async sendVerificationOTP({ email, otp, type }) {
+
+                let subject = "Code de vérification [Laboratoire]"
+
+                if (type === "sign-in") {
+                    subject = "Connexion avec OTP"
+
+                } else if (type === "forget-password") {
+                    subject = "Réinitialisation de mot de passe"
+                }
+
+                await sendEmail({ to: email, subject, otp })
+            }
+
+        }),
+        nextCookies(),
+    ]
 })
